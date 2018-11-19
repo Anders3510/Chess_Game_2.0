cdf77c40a206d6e4c5e99b37bcdbd82a3b48ebde
Anders <anders@nntq.net>
2018-11-19 08:23:35 +0100

    Added basic classes and simple functions
    Added functions to calculate the piece's moving angle and return a boolean representing whether the move is approved.

--- ChessGame2.0/ChessGame2.0/Program.cs
+++ ChessGame2.0/ChessGame2.0/Program.cs
@@ -1,12 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ChessGame2._0
+namespace ChessGame2
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+
+		}
+	}
+
+	class ChessGame
+	{
+
+		Piece[,] board = new Piece[8, 8];
+
+		//Coords array: { y1, y2, x1, x2 }
+
+		enum PieceColor
+		{
+			white,
+			black
+		}
+
+		abstract class Piece
+		{
+			PieceColor color { get; }
+
+			public abstract bool CalculateAngle(int[] coords);
+
+			//Work in progress
+			public bool MovePiece(int[] coords)
+			{
+				return true;
+			}
+		}
+
+		class Rook : Piece
+		{
+			public override bool CalculateAngle(int[] coords)
+			{
+				if (Math.Abs(coords[2] - coords[3]) / Math.Abs(coords[0] - coords[1]) == 0)
+					return true;
+				else
+					return false;
+			}
+		}
+
+		class Bishop : Piece
+		{
+			public override bool CalculateAngle(int[] coords)
+			{
+				if (Math.Abs(coords[2] - coords[3]) / Math.Abs(coords[0] - coords[1]) == 1)
+					return true;
+				else
+					return false;
+			}
+		}
+
+		class Queen : Piece
+		{
+			public override bool CalculateAngle(int[] coords)
+			{
+				int angle = Math.Abs(coords[2] - coords[3]) / Math.Abs(coords[0] - coords[1]);
+				if(angle == 1 || angle == 0)
+					return true;
+				else
+					return false;
+			}
+		}
+
+		class King : Piece
+		{
+			//Always returns true because the king can only move 1 tile in any direction.
+			public override bool CalculateAngle(int[] coords)
+			{
+				return true;
+			}
+		}
+
+		class Pawn : Piece
+		{
+			public override bool CalculateAngle(int[] coords)
+			{
+				throw new NotImplementedException();
+			}
 		}
 	}
 }
